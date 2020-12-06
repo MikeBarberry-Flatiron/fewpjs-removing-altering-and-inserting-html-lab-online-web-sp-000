423d1a464b64b102e6a97a83433fc73386f37daf
Mike Barberry <mikebarberry@protonmail.com>
2020-12-06 01:30:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,4 +9,8 @@ newHeader.innerHTML = "Mike is the champion"
 
 newHeader.style.color = "blue"
 
+newHeader.addEventListener('click', function(event) {
+  alert('I was clicked')
+})
+
 document.body.appendChild(newHeader)
