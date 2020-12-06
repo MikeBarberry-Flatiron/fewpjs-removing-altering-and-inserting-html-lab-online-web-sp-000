b17fe14b9e0543831d0c21a1f18eb8d0659f78e2
Mike Barberry <mikebarberry@protonmail.com>
2020-12-06 01:26:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const newHeader = document.createElement("h1")
 newHeader.id = "victory"
 
 newHeader.innerHTML = "Mike is the champion"
+
+newHeader.style.color = "blue"
+
+document.body.appendChild(newHeader)
